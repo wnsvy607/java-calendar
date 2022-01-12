fba76cb8e9ea69ba1c5e8960a9f00f27d7e0281c
wnsvy607 <wnsvy607@naver.com>
2022-01-13 05:20:45 +0900

    for 문으로 반복횟수, 월을 입력, 몇일인지 출력,

--- src/jp/calendar/Calendar.java
+++ src/jp/calendar/Calendar.java
@@ -1,13 +1,43 @@
 package jp.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
-	public static void main(String[] args) {
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysOfMonth(int month) {
+		if(month < 1 || month >12)
+			return 0;
+		return MAX_DAYS[month - 1];
+	}
+
+	public void printSampleCalendar() {
 		System.out.println("일	월	화	수	목	금	토");
 		System.out.println("----------------------------------------------------");
 		System.out.println("1	2	3	4	5	6	7");
 		System.out.println("8	9	10	11	12	13	14");
 		System.out.println("15	16	17	18	19	20	21");
 		System.out.println("22	23	24	25	26	27	28");
+	}
+
+	public static void main(String[] args) {
+		int[] arr;
+		Scanner in = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		System.out.println("반복횟수를 입력하세요.");
+		int num = in.nextInt();
+		System.out.println("월을 입력하세요.");
+
+		for (int i = 0; i < num; i++) {
+			int month = in.nextInt();
+			if(cal.getMaxDaysOfMonth(month) != 0)
+				System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+			else
+				System.out.printf("유효하지 않은 숫자입니다.\n");
+		}
+		cal.printSampleCalendar();
+		in.close();
 	}
 }
